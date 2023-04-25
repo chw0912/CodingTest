d37274ea97d218a628b9452f2667019c9d5e12e1
CHUIWWWW <95081400+CHUIWWWW@users.noreply.github.com>
2023-04-25 17:05:15 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - String, Time: 153 ms, Memory: 58,280 KB -BaekjoonHub

--- SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.py
+++ SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.py
@@ -7,11 +7,7 @@ for t in range(1, T+1):
 
     a = ex.find(tc)
     cnt = 1
-    while True:
-        if ex[a+1:].find(tc) != -1:
+    while ex[a+1:].find(tc) != -1:
             a = ex[a+1:].find(tc) + a + 1
             cnt += 1
-            continue
-        else:
-            break
     print(f'#{t} {cnt}')
