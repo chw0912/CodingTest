96ec916df82a1830bed57727424e4405b002edf1
CHUIWWWW <95081400+CHUIWWWW@users.noreply.github.com>
2023-05-29 20:33:20 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
@@ -2,6 +2,6 @@ def solution(absolutes, signs):
     answer = 123456789
     for i in range(len(absolutes)):
         if not signs[i]:
-            absolutes[i] = -1 * absolutes[i]
+            absolutes[i] = -absolutes[i]
     
     return sum(absolutes)
