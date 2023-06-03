a46dd1512adc169bbdcab30d216443f16d239a7b
CHUIWWWW <95081400+CHUIWWWW@users.noreply.github.com>
2023-06-03 15:58:18 +0900

    [level 2] Title: 짝지어 제거하기, Time: 159.75 ms, Memory: 14.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,15 @@
+def solution(s):
+    L = []
+    
+    for i in s:
+        if len(L) == 0:
+            L.append(i)
+        else:
+            if L[-1] == i:
+                L.pop()
+            else:
+                L.append(i)
+    if len(L) == 0:
+        return 1
+
+    return 0
