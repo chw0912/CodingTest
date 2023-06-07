97b496accc77c3e18167b30b25f4efb73e22de49
CHUIWWWW <95081400+CHUIWWWW@users.noreply.github.com>
2023-06-07 17:56:47 +0900

    [level 2] Title: 튜플, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/64065. 튜플/튜플.py
@@ -0,0 +1,16 @@
+from collections import Counter
+def solution(s):
+    answer = []
+    
+    s=list(s)
+    num = ''
+    for i in range(1,len(s)-1):
+              
+        if s[i].isdigit() == True:
+            num += s[i]
+        elif s[i-1].isdigit() == True and s[i] == ',' or s[i] == '}' :
+            answer.append(int(num))
+            num = ''
+    answer = Counter(answer)
+    
+    return sorted(answer, key=lambda i : answer[i],reverse=True)
