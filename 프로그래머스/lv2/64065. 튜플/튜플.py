3275adc3c5719507514c5cab74d3f1a59a1307f1
CHUIWWWW <95081400+CHUIWWWW@users.noreply.github.com>
2023-06-07 18:01:23 +0900

    [level 2] Title: 튜플, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.py
+++ 프로그래머스/lv2/64065. 튜플/튜플.py
@@ -1,11 +1,9 @@
 from collections import Counter
 def solution(s):
     answer = []
-    
     s=list(s)
     num = ''
-    for i in range(1,len(s)-1):
-              
+    for i in range(1,len(s)-1):  
         if s[i].isdigit() == True:
             num += s[i]
         elif s[i-1].isdigit() == True and s[i] == ',' or s[i] == '}' :
