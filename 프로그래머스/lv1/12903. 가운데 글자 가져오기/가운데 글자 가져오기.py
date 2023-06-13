3c8eac97b0202f077425bab154d836328d418e2b
CHUIWWWW <95081400+CHUIWWWW@users.noreply.github.com>
2023-06-13 19:07:35 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
@@ -1,9 +1,6 @@
 def solution(s):
     answer = ''
     
-    if len(s) % 2 == 1:
-        answer += s[len(s)//2]
-    else:
-        answer += s[len(s)//2-1:len(s)//2+1]
     
-    return answer
+    
+    return s[(len(s)-1)//2:len(s)//2+1]
