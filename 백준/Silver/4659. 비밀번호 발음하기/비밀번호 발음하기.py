044dc538c0b9c4d40f53e49b27b202d0da429e1c
CHUIWWWW <95081400+CHUIWWWW@users.noreply.github.com>
2023-07-19 17:09:04 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 120 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.py
@@ -0,0 +1,43 @@
+# Silver 5 4659. 비밀번호 발음하기
+
+vowel = ['a', 'e', 'i', 'o', 'u']
+
+while(1):
+    word = input()
+
+    if word == 'end':
+        break
+    # 모음 1개 이상인지 확인하기
+    v_flag = 0
+
+    # 자음, 모음 3번 연속 되는지 확인하기
+    v_cnt = 0
+    c_cnt = 0
+
+    # 같은 글자 2번 연속되는지 확인하기
+    err = 0
+
+    for i in range(len(word)):
+        if i > 0:
+            if word[i] == word[i-1]:
+                if word[i] != 'e' and word[i] != 'o':
+                    err = 1
+                    break
+        if word[i] in vowel:
+            v_flag = 1
+            v_cnt += 1
+            c_cnt = 0
+            if v_cnt == 3:
+                err = 1
+                break
+        else:
+            v_cnt = 0
+            c_cnt += 1
+            if c_cnt == 3:
+                err = 1
+                break
+    if err != 1 and v_flag == 1:
+        print(f"<{''.join(word)}> is acceptable.")
+    else:
+        print(f"<{''.join(word)}> is not acceptable.")
+
