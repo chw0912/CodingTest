23cf7463de65658acf11261eaa11d20510a8aec5
CHUIWWWW <95081400+CHUIWWWW@users.noreply.github.com>
2023-09-19 17:59:36 +0900

    [unrated] Title: 콜라 문제, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
@@ -0,0 +1,10 @@
+def solution(a, b, n):
+    answer = 0
+    
+    while n >= a:
+        answer += (n//a)*b
+        tmp = (n//a)*b
+        n = n-(n//a)*a+tmp
+        print(n)
+    
+    return answer
