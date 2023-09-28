99cbed641e9d1f110d5cc9852642102a7c381b1a
CHUIWWWW <95081400+CHUIWWWW@users.noreply.github.com>
2023-09-28 17:23:12 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.py
+++ 프로그래머스/lv1/42889. 실패율/실패율.py
@@ -1,23 +1,13 @@
-# def solution(N, stages):
-#     answer = {}
-    
-#     members = len(stages)
-#     for i in range(1,N+1):
-#         num = stages.count(i+1)
-#         answer[i] = num/members
-#         members -= num
-    
-#     return sorted(answer, key=lambda i : answer[i]+1,reverse=True)
-
 def solution(N, stages):
-    level = {}
-    t = len(stages)
-
-    for i in range(1, N+1):
-        if t == 0: 
-            level[i] = 0
+    answer = {}
+    
+    members = len(stages)
+    for i in range(1,N+1):
+        if members != 0:
+            num = stages.count(i)
+            answer[i] = num/members
+            members -= num
         else:
-            level[i] = stages.count(i)/t
-            t -= stages.count(i)
-    print(level)
-    return sorted(level, key=lambda x:level[x], reverse=True)
+            answer[i]=0
+    return sorted(answer, key=lambda i : answer[i],reverse=True)
+
