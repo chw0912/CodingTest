a7c4db64c01bf7e964ec34a84b995d00623480b3
CHUIWWWW <95081400+CHUIWWWW@users.noreply.github.com>
2024-03-05 15:46:35 +0900

    [Silver V] Title: 막대기, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.py
@@ -0,0 +1,15 @@
+# Silver 5. 막대기
+
+X = int(input())
+
+stick = [64, 32, 16, 8, 4, 2, 1]
+count = 0
+
+for i in range(len(stick)):
+    if X >= stick[i]:
+        count += 1
+        X -= stick[i]
+
+    if X == 0:
+        break
+print(count)
