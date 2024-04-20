7005e4c3e24736f98874f80b657eb8a3df510cba
CHUIWWWW <95081400+CHUIWWWW@users.noreply.github.com>
2024-04-20 14:49:27 +0900

    [Silver IV] Title: 수 찾기, Time: 208 ms, Memory: 143076 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -4,13 +4,15 @@ import sys
 
 N = int(sys.stdin.readline())
 
-A = sorted(list(map(int, sys.stdin.readline().split())))
+A = set(map(int, sys.stdin.readline().split()))
 
 M = int(sys.stdin.readline())
 
 X = list(map(int, sys.stdin.readline().split()))
 
+# X의 각 원소별 탐색
 for num in X:
+    # num이 A안에 있으면 1, 없으면 0 출력
     if num in A:
         print(1)
     else:
