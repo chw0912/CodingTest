4e2d7ca81f5447b0dc86d120d35b735642d0b5cd
CHUIWWWW <95081400+CHUIWWWW@users.noreply.github.com>
2024-06-10 23:13:58 +0900

    [level 1] Title: 덧칠하기, Time: 1.17 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -1,9 +1,11 @@
 def solution(n, m, section):
     answer = 0
-    paint = section[0] + m - 1
-    cnt = 1
+    paint = section[0] + m - 1 # 맨 처음 구역 + m - 1 까지의 구역을 페인트 칠하기
+    cnt = 1 # 페인트 칠한 횟수
     
+    # 주어진 원소들로만 페인트 칠하기
     for s in section:
+        # 만약 페인트 칠한 구역보다 s가 크면 s에서 다시 m - 1 만큼 페인트 칠하기 
         if paint < s:
             paint = s + m - 1
             cnt += 1
