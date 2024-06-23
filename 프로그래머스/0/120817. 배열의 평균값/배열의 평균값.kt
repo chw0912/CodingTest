cd9b44927d4254c52ae545c3738552f1f6de4f46
CHUIWWWW <95081400+CHUIWWWW@users.noreply.github.com>
2024-06-23 20:24:43 +0900

    [level 0] Title: 배열의 평균값, Time: 14.89 ms, Memory: 62.7 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.kt
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.kt
@@ -1,11 +1,3 @@
 class Solution {
-    fun solution(numbers: IntArray): Double {
-        var answer: Double = 0.0
-        
-        for (i in 0..numbers.size-1) {
-            answer += numbers[i]
-        }
-        
-        return answer/numbers.size
-    }
+    fun solution(numbers: IntArray) = numbers.average()
 }
