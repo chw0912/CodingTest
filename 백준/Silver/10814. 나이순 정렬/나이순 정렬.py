f5de0cfcfacd9b69a09f92494752174e6b6cb7f4
CHUIWWWW <95081400+CHUIWWWW@users.noreply.github.com>
2024-08-18 15:01:45 +0900

    [Silver V] Title: 나이순 정렬, Time: 328 ms, Memory: 135496 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -1,12 +1,13 @@
 # Silver 5. 나이순 정렬
-N = int(input())
+import sys
+N = int(sys.stdin.readline().strip())
 judge = []
 
 
 for i in range(1, N + 1):
-    age, name = input().split()
+    age, name = sys.stdin.readline().split()
     judge.append((int(age), i, name))
-judge.sort(key=lambda x: (x[0],x[1]))
+judge.sort(key=lambda x: (x[0], x[1]))
 
 for age,_,name in judge:
     print(age, name)
