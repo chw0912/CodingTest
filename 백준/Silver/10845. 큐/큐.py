ff8be0fabd756e02a9613f3b81ce8efdf2f5d72e
CHUIWWWW <95081400+CHUIWWWW@users.noreply.github.com>
2024-08-25 14:46:24 +0900

    [Silver IV] Title: 큐, Time: 124 ms, Memory: 112020 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.py
+++ 백준/Silver/10845. 큐/큐.py
@@ -9,29 +9,16 @@ N = int(input().rstrip())
 queue = []
 
 for _ in range(N):
-    command = input().rstrip()
-    if "push" in command:
-        queue.append(command.split()[1])
-    elif "pop" == command:
-        if queue:
-            pop_num = queue.pop(0)
-            print(pop_num)
-        else:
-            print(-1)
-    elif "size" == command:
+    command = input().rstrip().split()
+    if command[0] == "push":
+        queue.append(command[1])
+    elif command[0] == "pop":
+        print(queue.pop(0) if queue else -1)
+    elif command[0] == "size":
         print(len(queue))
-    elif "empty" == command:
-        if queue:
-            print(0)
-        else:
-            print(1)
-    elif "front" == command:
-        if queue:
-            print(queue[0])
-        else:
-            print(-1)
-    elif "back" == command:
-        if queue:
-            print(queue[-1])
-        else:
-            print(-1)
+    elif command[0] == "empty":
+        print(0 if queue else 1)
+    elif command[0] == "front":
+        print(queue[0] if queue else -1)
+    elif command[0] == "back":
+        print(queue[-1] if queue else -1)
