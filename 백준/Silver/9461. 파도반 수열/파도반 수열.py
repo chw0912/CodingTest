f264cb151823cd7cadfbe3c9bd6e70d52b24abdf
CHUIWWWW <95081400+CHUIWWWW@users.noreply.github.com>
2024-08-29 17:53:52 +0900

    [Silver III] Title: 파도반 수열, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.py
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -5,13 +5,13 @@ input = sys.stdin.readline
 
 T = int(input())
 
+p = [0] * 101
+for i in range(1, 101):
+    if i <= 3:
+        p[i] = 1
+    else:
+        p[i] = p[i - 2] + p[i - 3]
+
 for _ in range(T):
     N = int(input())
-    p = [0] * (N+1)
-
-    for i in range(1, N+1):
-        if i <= 3:
-            p[i] = 1
-        else:
-            p[i] = p[i-2] + p[i-3]
     print(p[N])
