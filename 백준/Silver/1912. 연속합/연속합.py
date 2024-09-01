e06cc8498bdc41fa7cffa6fb635dd09422ea0f74
CHUIWWWW <95081400+CHUIWWWW@users.noreply.github.com>
2024-09-01 12:36:23 +0900

    [Silver II] Title: 연속합, Time: 104 ms, Memory: 120860 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -0,0 +1,14 @@
+# Silver 2. 연속합
+
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+lst = list(map(int, input().split()))
+
+for i in range(1, n):
+    lst[i] = max(lst[i], lst[i] + lst[i-1])
+    
+print(max(lst))
