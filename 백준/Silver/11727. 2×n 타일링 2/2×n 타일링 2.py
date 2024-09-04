eece5a1f5af9320c2f40457dd9c6e3e917ec82df
CHUIWWWW <95081400+CHUIWWWW@users.noreply.github.com>
2024-09-04 11:47:54 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 96 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -5,13 +5,13 @@ input = sys.stdin.readline
 
 n = int(input())
 
-tiles = [0] * 1001
+tiles = [0] * (n+1)
 
-for i in range(1,1001):
+for i in range(1, n+1):
     if i == 1:
         tiles[i] = 1
     elif i == 2:
         tiles[i] = 3
     else:
-        tiles[i] = (tiles[i-1] + tiles[i-2]) * 2 - tiles[i-1]
+        tiles[i] = tiles[i-1] + tiles[i-2]*2
 print(tiles[n] % 10007)
