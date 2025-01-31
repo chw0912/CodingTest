b95850cf17ac2271159d5bad00b35c1b8aa3c95b
CHUIWWWW <95081400+CHUIWWWW@users.noreply.github.com>
2025-01-31 15:45:47 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 3.30 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -2,13 +2,10 @@ class Solution {
     public String solution(String phone_number) {
         String answer = "";
         
-        for (int i=0;i<phone_number.length();i++) {
-            if (i >= phone_number.length()-4) {
-                answer += phone_number.charAt(i);
-            } else {
-                answer += "*";
-            }
-        }
+        for (int i=0;i<phone_number.length() - 4;i++) 
+            answer += "*";
+        
+        answer += phone_number.substring(phone_number.length()-4);
         
         return answer;
     }
