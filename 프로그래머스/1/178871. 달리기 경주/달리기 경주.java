304e704cfaf105f7f8481bce65f616cc8c238604
CHUIWWWW <95081400+CHUIWWWW@users.noreply.github.com>
2025-02-01 14:22:16 +0900

    [level 1] Title: 달리기 경주, Time: 321.36 ms, Memory: 360 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] players, String[] callings) {
+        String[] answer = {};
+        Map<String,Integer> ranking = new HashMap<>();
+        
+        for (int i = 0;i < players.length;i++) {
+            ranking.put(players[i], i);
+        }
+        
+        for (String player : callings) {
+            int playerRank = ranking.get(player);
+            
+            String frontPlayer = players[playerRank - 1];
+            
+            ranking.replace(frontPlayer,playerRank);
+            players[playerRank] = frontPlayer;
+            
+            ranking.replace(player,playerRank-1);
+            players[playerRank-1] = player;
+        }
+        
+        return players;
+    }
+}
