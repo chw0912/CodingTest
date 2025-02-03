750caa960e6c8fbd49ff4347568b306de79dfed3
CHUIWWWW <95081400+CHUIWWWW@users.noreply.github.com>
2025-02-03 13:19:32 +0900

    [level 1] Title: 문자열 나누기, Time: 2.04 ms, Memory: 85.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        char[] str = s.toCharArray();
+        List<String> strList = new ArrayList<>();
+        char first = str[0];
+        int same = 0;
+        int diff = 0;
+        
+        for (int i=0;i<str.length;i++) {
+            if (first == str[i]) {
+                same += 1;
+            } else if(first != str[i]) {
+                diff += 1;
+            }
+            
+            if (same == diff) {
+                same = 0;
+                diff = 0;
+                answer += 1;
+                if (i+1 < str.length) {
+                    first = str[i+1];
+                }
+            }
+            
+            if (same != diff && i == str.length-1) {
+                answer += 1;
+            }
+        }
+        
+        return answer;
+    }
+}
