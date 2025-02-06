8e1f0ea8e2ffab1db7d5e74b46804aae51f93c72
CHUIWWWW <95081400+CHUIWWWW@users.noreply.github.com>
2025-02-06 16:35:02 +0900

    [level 2] Title: 주식가격, Time: 20.11 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -5,11 +5,9 @@ class Solution {
         for (int i = 0;i < answer.length; i++) {
             answer[i] = 0;
             for (int j = i + 1; j<answer.length; j++) {
-                if (prices[i] > prices[j]) {
-                    answer[i]++;
+                answer[i]++;
+                if (prices[i] > prices[j]) { 
                     break;
-                } else {
-                    answer[i]++;
                 }
             }
         }
