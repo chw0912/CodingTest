6b5876b4c3242d668acf330ee791fd171739dc51
CHUIWWWW <95081400+CHUIWWWW@users.noreply.github.com>
2025-02-08 22:04:10 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.05 ms, Memory: 93.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int solution(int[] cards) {
+        int answer = 0;
+        
+        boolean[] visited = new boolean[cards.length];
+        
+        int group1 = 0;
+        int group2 = 0;
+        
+        for (int i = 0; i < cards.length; i++) {
+            int cnt = 0; 
+            int idx = i;
+            while (!visited[idx]) {
+                visited[idx] = true;
+                idx = cards[idx]-1;
+                cnt++;
+            }
+
+            if (group1 == cards.length) {
+                group1 = 0;
+                break;
+            }
+            
+            
+            if (group1 < Math.max(group1, cnt)) {
+                int tmp = group1;
+                group1 = cnt;
+                group2 = tmp;
+                
+            } else if (group2 < Math.max(group2, cnt)) {
+                group2 = cnt;
+            }
+        }
+        answer = group1 * group2;
+        
+        return answer;
+    }
+}
