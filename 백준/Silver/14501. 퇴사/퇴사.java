c442e4d7bed1ef98b5df91f055c48587220ddd57
Huiwoong Choi <95081400+chw0912@users.noreply.github.com>
2025-03-09 14:52:23 +0900

    [Silver III] Title: 퇴사, Time: 176 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -0,0 +1,27 @@
+// Silver 3. 퇴사
+
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] T = new int[N];
+        int[] P = new int[N];
+        for (int i = 0; i < N; i++) {
+            T[i] = sc.nextInt();
+            P[i] = sc.nextInt();
+        }
+        int[] dp = new int[N+1];
+
+        for (int i = 0; i < N; i++) {
+            if (i+T[i] <= N) {
+                dp[i+T[i]] = Math.max(dp[i+T[i]], dp[i] + P[i]);
+            }
+            dp[i+1] = Math.max(dp[i+1], dp[i]);
+        }
+        System.out.println(dp[N]);
+    }
+}
