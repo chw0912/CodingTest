143d34c8a27f5cb5b30ba9fcb4f23ae8cfdfa5c0
Huiwoong Choi <95081400+chw0912@users.noreply.github.com>
2025-04-12 17:53:42 +0900

    [Silver III] Title: 퇴사, Time: 104 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,27 +1,38 @@
-// Silver 3. 퇴사
-
-import java.util.*;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
 
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static int N; // 최대 상담 가능일
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int[] T = new int[N];
-        int[] P = new int[N];
-        for (int i = 0; i < N; i++) {
-            T[i] = sc.nextInt();
-            P[i] = sc.nextInt();
-        }
-        int[] dp = new int[N+1];
+    public static void main(String[] args) throws IOException {
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+
+        int[] dp = new int[N + 1];
 
         for (int i = 0; i < N; i++) {
-            if (i+T[i] <= N) {
-                dp[i+T[i]] = Math.max(dp[i+T[i]], dp[i] + P[i]);
+            st = new StringTokenizer(br.readLine());
+            int T = Integer.parseInt(st.nextToken());
+            int P = Integer.parseInt(st.nextToken());
+
+            // 상담 완료 날짜가 N을 넘기지 않는다면
+            if (i + T <= N) {
+                // 해당 완료 날짜의 데이터와 현재 날짜에서
+                dp[i+T] = Math.max(dp[i+T], dp[i] + P);
             }
-            dp[i+1] = Math.max(dp[i+1], dp[i]);
+
+            // 오늘 일한 금액을 다음날과 비교
+            dp[i+1] = Math.max(dp[i], dp[i+1]);
         }
-        System.out.println(dp[N]);
+
+        bw.write(String.valueOf(dp[N]));
+        bw.flush();
     }
+
+
 }
