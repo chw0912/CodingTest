4d0c79145244f738f7abd7a3466197639084b293
Huiwoong Choi <95081400+chw0912@users.noreply.github.com>
2025-04-12 18:59:16 +0900

    [Silver III] Title: 퇴사, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -4,7 +4,6 @@ import java.util.StringTokenizer;
 public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
     static int N; // 최대 상담 가능일
 
@@ -22,16 +21,15 @@ public class Main {
 
             // 상담 완료 날짜가 N을 넘기지 않는다면
             if (i + T <= N) {
-                // 해당 완료 날짜의 데이터와 현재 날짜에서
+                // 해당 완료 날짜의 데이터와 현재 날짜와 완료된 날의 금액을 더하여 큰값 저장
                 dp[i+T] = Math.max(dp[i+T], dp[i] + P);
             }
 
-            // 오늘 일한 금액을 다음날과 비교
+            // 오늘 일한 금액을 다음날로
             dp[i+1] = Math.max(dp[i], dp[i+1]);
         }
 
-        bw.write(String.valueOf(dp[N]));
-        bw.flush();
+        System.out.println(dp[N]);
     }
 
 
