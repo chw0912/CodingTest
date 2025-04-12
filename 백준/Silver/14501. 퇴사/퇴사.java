167792cef37216316cd3e62193d7694b09b08c92
Huiwoong Choi <95081400+chw0912@users.noreply.github.com>
2025-04-12 19:08:28 +0900

    [Silver III] Title: 퇴사, Time: 100 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,18 +1,19 @@
 import java.io.*;
 import java.util.StringTokenizer;
 
+
+// 14501 [S3] 퇴사
 public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static int N; // 최대 상담 가능일
+    static int[] dp;
 
     public static void main(String[] args) throws IOException {
 
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(br.readLine());
 
-        int[] dp = new int[N + 1];
+        dp = new int[N + 1];
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
@@ -22,11 +23,11 @@ public class Main {
             // 상담 완료 날짜가 N을 넘기지 않는다면
             if (i + T <= N) {
                 // 해당 완료 날짜의 데이터와 현재 날짜와 완료된 날의 금액을 더하여 큰값 저장
-                dp[i+T] = Math.max(dp[i+T], dp[i] + P);
+                dp[i + T] = Math.max(dp[i + T], dp[i] + P);
             }
 
             // 오늘 일한 금액을 다음날로
-            dp[i+1] = Math.max(dp[i], dp[i+1]);
+            dp[i + 1] = Math.max(dp[i], dp[i + 1]);
         }
 
         System.out.println(dp[N]);
