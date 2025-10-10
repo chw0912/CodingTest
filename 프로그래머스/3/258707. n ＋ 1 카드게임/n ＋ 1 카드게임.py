070a18e1209d8539bdf62b65834daf85cb581c5c
Huiwoong Choi <95081400+chw0912@users.noreply.github.com>
2025-10-10 21:48:12 +0900

    [level 3] Title: n + 1 카드게임, Time: 9.95 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.py
@@ -0,0 +1,43 @@
+from collections import deque
+
+def solution(coin, cards):
+    answer = 1
+    n = len(cards)
+    s = n//3
+    
+    hand = deque(cards[:s])
+    deck = deque(cards[s:])
+    
+    pending = []
+    
+    while coin >= 0 and deck:
+        
+        pending.append(deck.popleft())
+        pending.append(deck.popleft())
+        
+        if check(hand,hand,n+1):
+            pass
+        elif coin >= 1 and check(hand, pending, n+1):
+            coin -= 1
+        elif coin >= 2 and check(pending, pending, n+1):
+            coin -= 2
+        else:
+            break
+        
+        answer += 1
+         
+    return answer
+
+def check(deck1, deck2, target):
+    
+    operand = set(deck2)
+    for card in deck1:
+        if target-card in operand:
+            deck1.remove(card)
+            deck2.remove(target-card)
+            return True
+    
+    return False
+        
+    
+    
