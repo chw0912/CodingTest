b4b9cedd78e31ef95bdf324c7425e2b9d37e538c
Huiwoong Choi <95081400+chw0912@users.noreply.github.com>
2025-11-21 17:16:29 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 146 ms, Memory: 62,080 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
@@ -1,34 +1,34 @@
+# 최대 상금
+
 def dfs(n):
     global ans
-    if n == N:
-        ans = max(ans, int(''.join(map(str,lst))))
+
+    if n == count:
+        ans = max(ans, int(''.join(map(str, lst))))
         return
 
-    #L개에서 2 개 뽑는 모든 조합(들을 교환)
     for i in range(L-1):
-        for j in range(i+1,L):
-            lst[i],lst[j] = lst[j],lst[i]
-
-            chk = int(''.join(map(str,lst)))*10+n
+        for j in range(i+1, L):
+            lst[i], lst[j] = lst[j], lst[i]
+            chk = int(''.join(map(str, lst)))*10 + n
             if chk not in v:
                 dfs(n+1)
                 v.append(chk)
-            lst[i],lst[j] = lst[j],lst[i]
+            lst[i], lst[j] = lst[j], lst[i]
+
 
 T = int(input())
 
 for t in range(1, T+1):
-    nums, cnt = input().split()
-
-    N = int(cnt)
-    lst = []
-
-    for ch in nums:
-        lst.append(int(ch))
+    numbers, count = map(int, input().split())
+    lst = list(str(numbers))
     L = len(lst)
-    ans = 0
     v = []
+    ans = 0
     dfs(0)
-    print(f'#{t} {ans}')
+
+    print(f"#{t} {ans}")
+
+
 
 
