f0381faeeaa686ae31cd1b40b9c57a90a3e51e17
Huiwoong Choi <95081400+chw0912@users.noreply.github.com>
2025-12-11 16:19:28 +0900

    [Gold II] Title: 환승, Time: 768 ms, Memory: 165964 KB -BaekjoonHub

---
+++ 백준/Gold/5214. 환승/환승.java
@@ -0,0 +1,81 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(out));
+    static StringTokenizer st;
+    static int N, K, M; // 역의 수, 서로 연결하는 역의 개수, 하이퍼튜브 개수
+    static ArrayList<ArrayList<Integer>> tubeStation = new ArrayList<>();
+    static Queue<Integer> queue = new LinkedList<>();
+    static int[] visited;
+    static int ans = -1;
+
+    public static void main(String[] args) throws IOException {
+        input();
+        solve();
+        output();
+    }
+
+    static void input() throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        visited = new int[N+M+1];
+        for ( int i = 0; i <= N+M+1; i++ ) {
+            tubeStation.add(new ArrayList<>());
+        }
+
+        for ( int m = 1; m <= M; m++ ) {
+            st = new StringTokenizer(br.readLine());
+            for ( int k = 0; k < K; k++ ) {
+                int station = Integer.parseInt(st.nextToken());
+                tubeStation.get(station).add(N+m);
+                tubeStation.get(N+m).add(station);
+            }
+        }
+    }
+
+    static void solve() {
+        bfs(1);
+    }
+
+    static void output() throws IOException {
+        bw.write(String.valueOf(ans));
+        bw.flush();
+    }
+
+    static void bfs(int x) {
+        queue.offer(x);
+        visited[1] = 1;
+
+        while( !queue.isEmpty() ) {
+            int station = queue.poll();
+            if ( station == N ) {
+                ans = visited[N];
+                break;
+            }
+
+            for ( int nextStation : tubeStation.get(station) ) {
+                if ( visited[nextStation] == 0 ) {
+                    if ( station > N ) {
+                        visited[nextStation] = visited[station];
+                    } else {
+                        visited[nextStation] = visited[station] + 1;
+                    }
+                    queue.offer(nextStation);
+                }
+            }
+        }
+    }
+
+
+}
+
