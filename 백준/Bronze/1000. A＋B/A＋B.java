a939747f3356b912e9ae60734e21f6283df7f164
Huiwoong Choi <95081400+chw0912@users.noreply.github.com>
2025-12-13 17:52:36 +0900

    [Bronze V] Title: A+B, Time: 72 ms, Memory: 11592 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(out));
+    static StringTokenizer st;
+    static int A, B;
+    public static void main(String[] args) throws IOException {
+        input();
+        solve();
+        output();
+    }
+
+    static void input() throws IOException {
+        st = new StringTokenizer(br.readLine());
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+        
+    }
+
+    static void solve() {
+
+    }
+
+    static void output() throws IOException {
+        bw.write(String.valueOf(A+B));
+        bw.flush();
+    }
+}
+
