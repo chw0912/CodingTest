2a1db9d27118cb3e04ef1e0c67d882d8b626e8a0
Huiwoong Choi <95081400+chw0912@users.noreply.github.com>
2025-12-17 21:25:50 +0900

    [Bronze V] Title: A/B, Time: 68 ms, Memory: 11708 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(out));
+    static StringTokenizer st;
+    static int A, B;
+    static double ans;
+
+    public static void main(String[] args) throws IOException {
+        input();
+        solve();
+        output();
+    }
+
+    static void input() throws IOException {
+        st = new StringTokenizer(br.readLine());
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+        ans = (double) A /B;
+
+    }
+
+    static void solve() {
+
+    }
+
+    static void output() throws IOException {
+        bw.write(String.valueOf(ans));
+        bw.flush();
+    }
+}
+
