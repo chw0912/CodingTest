7fe76b4b4cce6acef1f649d7fffdf4f024934818
Huiwoong Choi <95081400+chw0912@users.noreply.github.com>
2025-12-26 18:15:38 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 68 ms, Memory: 11652 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(out));
+    static StringTokenizer st;
+    static int N;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        input();
+        solve();
+        output();
+    }
+
+    static void input() throws IOException {
+        N = Integer.parseInt(br.readLine());
+    }
+
+    static void solve() {
+        for ( int i = 1; i <= N; i++ ) {
+            for ( int j = 1; j <= i; j++ ) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+    }
+
+    static void output() throws IOException {
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
+
