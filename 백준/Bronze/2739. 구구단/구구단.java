673863e62e80a7773d92268c1a2af859f80b027b
Huiwoong Choi <95081400+chw0912@users.noreply.github.com>
2025-12-31 15:02:30 +0900

    [Bronze V] Title: 구구단, Time: 64 ms, Memory: 11592 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(out));
+    static StringTokenizer st;
+    static int N;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        input();
+        solve();
+        output();
+    }
+
+    static void input() throws IOException {
+        N = Integer.parseInt(br.readLine());  
+    }
+
+    static void solve() {
+        for ( int i = 1; i <= 9; i++ ) {
+            sb.append(N).append(" * ").append(i).append(" = ").append(N*i).append("\n");
+        }
+    }
+
+    static void output() throws IOException {
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
