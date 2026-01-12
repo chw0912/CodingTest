fa91433abe33ceb745a64926d1c92aa46c36bd36
Huiwoong Choi <95081400+chw0912@users.noreply.github.com>
2026-01-12 12:24:38 +0900

    [Gold V] Title: 점프 게임, Time: 420 ms, Memory: 44056 KB -BaekjoonHub

--- 백준/Gold/15558. 점프 게임/점프 게임.java
+++ 백준/Gold/15558. 점프 게임/점프 게임.java
@@ -1,7 +1,6 @@
 // G5. 점프게임
 
 import java.io.*;
-import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
@@ -15,7 +14,6 @@ public class Main {
     static int N, k;
     static int[][] map;
     static boolean[][] visited;
-    static ArrayDeque<int[]> dq = new ArrayDeque<>();
     static int ans;
 
     public static void main(String[] args) throws IOException {
@@ -39,7 +37,7 @@ public class Main {
     }
 
     static void solve() {
-        bfs();
+        if(dfs(0,0,0)) ans = 1;
     }
 
     static void output() throws IOException {
@@ -51,41 +49,50 @@ public class Main {
 
     }
 
-    static void bfs() {
-        dq.offer(new int[] {0, 0, 0});
-        visited[0][0] = true;
+    static boolean dfs(int line, int idx, int time) {
+        boolean isPossible = false;
 
-        while (!dq.isEmpty()) {
-            int[] cur = dq.poll();
-            int line = cur[0];
-            int idx = cur[1];
-            int time = cur[2];
-            
-            int[] nextLines = {line, line, (line+1) % 2};
-            int[] nextIndexes = {idx+1, idx -1, idx+k};
-            
-            for (int i = 0; i < 3; i++) {
-                int nextLine = nextLines[i];
-                int nextIdx = nextIndexes[i];
-                int nextTime = time + 1;
-                
-                // 1. N 이상으로 넘어가면 성공 
-                if (nextIdx >= N) {
-                    ans = 1;
-                    return;
-                }
-                
-                // 2. 이동 불가능 조건 체크
-                if (nextIdx < 0) continue;
-                if (nextIdx < nextTime) continue;
-                if (map[nextLine][nextIdx] == 0) continue;
-                if (visited[nextLine][nextIdx]) continue;
-                
-                // 3. 이동 가능한 좌표 큐에 삽입
-                visited[nextLine][nextIdx] = true;
-                dq.offer(new int[] {nextLine, nextIdx, nextTime});
-            }
+        // 게임 클리어
+        if ( idx >= N ) {
+            isPossible = true;
+            return isPossible;
         }
-    }
+        
+        // 방문 처리
+        if ( idx >= 0 && visited[line][idx]) {
+            return isPossible;
+        }
+        
+        // 범위를 벗어나는 경우
+        if ( idx < 0 ) {
+            return isPossible;
+        }
+        // 해당 칸은 위험한 칸일 경우
+        if (map[line][idx] == 0) {
+            return isPossible;
+        }
 
+        // 1초에 한칸씩 사라지는 기능
+        if ( idx < time ) {
+            return isPossible;
+        }
+
+        // 방문 처리
+        visited[line][idx] = true;
+
+        // 뒤로 한칸 이동할 경우
+        if (dfs(line, idx-1, time+1)) {
+            return true;
+        }
+        // 라인을 변경할 경우
+        if (dfs((line+1) % 2, idx+k, time+1)) {
+            return true;
+        }
+        // 앞으로 한칸 이동할 경우
+        if (dfs(line, idx+1, time+1)) {
+            return true;
+        }
+
+        return false;
+    }
 }
