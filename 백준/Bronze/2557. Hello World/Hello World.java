8e95b09ce61eb8843b39a29cde4a4e2c4cecce88
Huiwoong Choi <95081400+chw0912@users.noreply.github.com>
2026-01-16 17:39:30 +0900

    [Bronze V] Title: Hello World, Time: 64 ms, Memory: 11396 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(out));
+    static StringTokenizer st;
+    static int A, B;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        input();
+        solve();
+        output();
+    }
+
+    static void input() throws IOException {
+    }
+
+    static void solve() {
+
+    }
+
+    static void output() throws IOException {
+        bw.write("Hello World!");
+        bw.flush();
+    }
+}
+
