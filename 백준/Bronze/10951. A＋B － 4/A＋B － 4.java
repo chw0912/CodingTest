c0ac858f39ad7dfe9e44ef85ee6da3140f0eec07
Huiwoong Choi <95081400+chw0912@users.noreply.github.com>
2026-01-23 23:41:58 +0900

    [Bronze V] Title: A+B - 4, Time: 68 ms, Memory: 11560 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(out));
+    static StringTokenizer st;
+    static int A, B;
+    static StringBuilder sb = new StringBuilder();
+
+
+    public static void main(String[] args) throws IOException {
+        input();
+        solve();
+        output();
+    }
+
+    static void input() throws IOException {
+        String str;
+        while ((str = br.readLine()) != null) {
+            
+            A = str.charAt(0) - 48;
+            B = str.charAt(2) - 48;
+
+            sb.append(A+B).append('\n');
+        }
+    }
+
+    static void solve() {
+
+    }
+
+    static void output() throws IOException {
+        bw.write(sb.toString());
+        bw.flush();
+
+    }
+}
+
