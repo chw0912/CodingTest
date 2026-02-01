72d1ce898450cff265702b0ee91ada66baea3a40
Huiwoong Choi <95081400+chw0912@users.noreply.github.com>
2026-02-01 23:20:21 +0900

    [Bronze V] Title: 개, Time: 56 ms, Memory: 11404 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(out));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        input();
+        solve();
+        output();
+    }
+
+    static void input() throws IOException {
+        sb.append("|\\_/|").append("\n");
+        sb.append("|q p|   /}").append("\n");
+        sb.append("( 0 )\"\"\"\\").append("\n");
+        sb.append("|\"^\"`    |").append("\n");
+        sb.append("||_/=\\\\__|").append("\n");
+    }
+
+    static void solve() {
+
+    }
+
+    static void output() throws IOException {
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
