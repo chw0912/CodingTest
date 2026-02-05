5df36023401f41844fe9351836569202e262c632
Huiwoong Choi <95081400+chw0912@users.noreply.github.com>
2026-02-05 17:39:31 +0900

    [Gold IV] Title: 알파벳, Time: 1004 ms, Memory: 12312 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,6 +1,4 @@
 import java.io.*;
-import java.util.ArrayDeque;
-import java.util.HashSet;
 import java.util.StringTokenizer;
 
 import static java.lang.System.in;
@@ -12,10 +10,10 @@ public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(out));
     static StringTokenizer st;
     static int R, C; // R: 행, C: 열
-    static String[][] board;
+    static char[][] board;
+    static boolean[] visited;
     static int[] dx = {0, 0, 1, -1};
     static int[] dy = {1, -1, 0, 0};
-    static ArrayDeque<int[]> dq = new ArrayDeque<>();
     static int ans;
 
     public static void main(String[] args) throws IOException {
@@ -28,15 +26,16 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
-        board = new String[R][C];
+        board = new char[R][C];
+        visited = new boolean[26];
 
         for ( int i = 0; i < R; i++ ) {
-            board[i] = br.readLine().split("");
+            board[i] = br.readLine().toCharArray();
         }
     }
 
     static void solve() {
-        dfs(0,0,0,new HashSet<>());
+        dfs(0,0,0);
     }
 
     static void output() throws IOException {
@@ -44,24 +43,22 @@ public class Main {
         bw.flush();
     }
 
-    static void dfs(int x, int y, int d, HashSet<String> set) {
-
-        if (set.contains(board[x][y])) {
+    static void dfs(int x, int y, int d) {
+        int index = board[x][y] - 'A';
+        if (visited[index]) {
             return;
         }
 
-        set.add(board[x][y]);
-
+        visited[index] = true;
         ans = Math.max(ans, d + 1);
 
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
             if (nx >= 0 && ny >= 0 && nx < R && ny < C) {
-                dfs(nx, ny, d + 1, set);
+                dfs(nx, ny, d + 1);
             }
         }
-
-        set.remove(board[x][y]);
+        visited[index] = false;
     }
 }
