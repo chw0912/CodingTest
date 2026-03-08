32d31db25776f58a2b18bfae3abf28b384a7c345
Huiwoong Choi <95081400+chw0912@users.noreply.github.com>
2026-03-08 23:22:17 +0900

    [level 1] Title: 중요한 단어를 스포 방지, Time: 4.31 ms, Memory: 9.52 MB -BaekjoonHub

---
+++ 프로그래머스/1/468370. 중요한 단어를 스포 방지/중요한 단어를 스포 방지.py
@@ -0,0 +1,34 @@
+def solution(message, spoiler_ranges):
+    answer = 0
+    
+    spoiler_message = [False] * len(message)
+    message_range = []
+    
+    start = 0
+    end = 0
+    
+    # 단어별 범위 구하기
+    while(end != len(message)):
+        if message[end] == " " or end == len(message)-1:
+            message_range.append((message[start:end+1].strip(), start, end-1))
+            start = end + 1
+        end += 1
+        
+    for start,end in spoiler_ranges:
+        for i in range(start,end+1):
+            spoiler_message[i] = True
+        
+    words = set()
+    for word, start, end in message_range:
+        is_spoiler = any(spoiler_message[i] for i in range(start, end+1))
+        if not is_spoiler:
+            words.add(word)
+    
+    result = 0
+    print(words)
+    for word, start, end in message_range:
+        if word not in words:
+            result+=1
+            words.add(word)
+    
+    return result
