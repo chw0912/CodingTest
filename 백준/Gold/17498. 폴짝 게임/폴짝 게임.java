647e0cd3e9597e5ad0e53dc679f530e9350f6789
Huiwoong Choi <95081400+chw0912@users.noreply.github.com>
2026-04-18 17:53:24 +0900

    [Gold V] Title: 폴짝 게임, Time: 420 ms, Memory: 47992 KB -BaekjoonHub

--- 백준/Gold/17498. 폴짝 게임/폴짝 게임.java
+++ 백준/Gold/17498. 폴짝 게임/폴짝 게임.java
@@ -43,30 +43,32 @@ public class Main {
     static void solve() {
         int r = 0; // 행 인덱스
         int c = 0; // 열 인덱스
+
+        // 기준 좌표 (r, c)
+        // 이동 좌표 (p, q)
         while (r < N) {
-            if(r == 0 || dp[r][c] != Integer.MIN_VALUE) {
-                for (int p = r+1; p < N && (p - r) <= D; p++) {
-                    int startQ = c, endQ = c;
-                    int distance = D - (p-r);
+            for (int p = r+1; p < N && (p - r) <= D; p++) {
+                // startQ : 다음 칸으로 이동하고 왼쪽으로 남은 거리
+                // endQ : 다음 칸으로 이동하고 오른쪽으로 남은 거리
+                int distance = D - (p-r);
+                int startQ = c - distance, endQ = c + distance;
 
-                    if (startQ - distance >= 0) {
-                        startQ -= distance;
-                    } else {
-                        startQ = 0;
-                    }
+                // 열의 범위를 넘어간 경우 양 끝 좌표로 초기화
+                if (startQ < 0) {
+                    startQ = 0;
+                }
 
-                    if (endQ + distance  < M) {
-                        endQ += distance;
-                    } else {
-                        endQ = M - 1;
-                    }
+                if (endQ  >= M){
+                    endQ = M - 1;
+                }
 
-                    for (int q = startQ; q <= endQ; q++) {
-                        dp[p][q] = Math.max(dp[p][q], dp[r][c] + (board[r][c] * board[p][q]));
+                // startQ 부터 endQ까지 돌면서 최대값 갱신
+                for (int q = startQ; q <= endQ; q++) {
+                    dp[p][q] = Math.max(dp[p][q], dp[r][c] + (board[r][c] * board[p][q]));
 
-                    }
                 }
             }
+
             c++;
             if(c == M) {
                 c = 0;
