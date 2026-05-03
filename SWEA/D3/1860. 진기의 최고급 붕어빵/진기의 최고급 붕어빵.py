3c4b6c689530c596032278a670b4897c1a7c9b51
Huiwoong Choi <95081400+chw0912@users.noreply.github.com>
2026-05-03 14:43:01 +0900

    [D3] Title: 진기의 최고급 붕어빵, Time: 300 ms, Memory: 64,896 KB -BaekjoonHub

--- SWEA/D3/1860. 진기의 최고급 붕어빵/진기의 최고급 붕어빵.py
+++ SWEA/D3/1860. 진기의 최고급 붕어빵/진기의 최고급 붕어빵.py
@@ -1,30 +1,31 @@
+#D3. 진기의 최고급 붕어빵
+
 T = int(input())
 
-result = list()
-
 for t in range(1, T+1):
-
     N, M, K = map(int, input().split())
 
     arrive = sorted(list(map(int, input().split())))
 
-    complete = 0
-
-    time = 0
     idx = 0
+    time = 0
+    fish_cake = 0
+    flag = True
 
-    while idx != N:
-        if time % M == 0 and time != 0:
-            complete += K
+    while time <= arrive[-1]:
 
-        if time >= arrive[idx]:
-            if complete >= 1:
-                complete -= 1
-                idx += 1
-            else:
-                print(f"#{t} Impossible")
+        if time != 0 and time % M == 0:
+            fish_cake += K
+
+        if arrive[idx] == time:
+            fish_cake -= 1
+            idx += 1
+            if fish_cake < 0:
+                flag = False
                 break
-
         time += 1
-    else:
+
+    if flag:
         print(f"#{t} Possible")
+    else:
+        print(f"#{t} Impossible")
