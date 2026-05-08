8134c12374261f55dda393e4512abd2866bd1099
Huiwoong Choi <95081400+chw0912@users.noreply.github.com>
2026-05-08 14:52:02 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - String, Time: 58 ms, Memory: 51,200 KB -BaekjoonHub

--- SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.py
+++ SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.py
@@ -1,13 +1,14 @@
+#D3. String
+
 T = 10
 
-for t in range(1, T+1):
-    N = int(input())
-    tc = input()
-    ex = input()
+for t in range(10):
+    time = int(input())
 
-    a = ex.find(tc)
-    cnt = 1
-    while ex[a+1:].find(tc) != -1:
-            a = ex[a+1:].find(tc) + a + 1
-            cnt += 1
-    print(f'#{t} {cnt}')
+    find = input()
+
+    string = input()
+
+    ans = len(string.split(find)) - 1
+
+    print(f'#{time} {ans}')
