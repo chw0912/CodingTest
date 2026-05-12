b064e8ec82e9b3ce61a64deab50bcd6edf43feb4
Huiwoong Choi <95081400+chw0912@users.noreply.github.com>
2026-05-12 16:35:30 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 121 ms, Memory: 60,544 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
@@ -1,34 +1,34 @@
-# 최대 상금
+# D3. 최대 상금
 
 def dfs(n):
     global ans
-
-    if n == count:
-        ans = max(ans, int(''.join(map(str, lst))))
+    
+    if n == changes:
+        ans = max(ans, int(''.join(map(str,lst))))
         return
 
     for i in range(L-1):
         for j in range(i+1, L):
             lst[i], lst[j] = lst[j], lst[i]
-            chk = int(''.join(map(str, lst)))*10 + n
+            chk = int(''.join(lst)) * 10 + n
+            
             if chk not in v:
                 dfs(n+1)
                 v.append(chk)
+            
             lst[i], lst[j] = lst[j], lst[i]
 
 
 T = int(input())
 
-for t in range(1, T+1):
-    numbers, count = map(int, input().split())
+for t in range(1, T + 1):
+
+    numbers, changes = map(int, input().split())
+
     lst = list(str(numbers))
+    ans = 0
     L = len(lst)
     v = []
-    ans = 0
     dfs(0)
-
-    print(f"#{t} {ans}")
-
-
-
-
+    
+    print(f'#{t} {ans}')
