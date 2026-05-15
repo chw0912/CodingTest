7f4db03d27f04fa2116188cb57244d9c97039150
hwsg0912@naver.com <hwsg0912@naver.com>
2026-05-15 17:04:09 +0900

    [Silver 1] 1681. 해밀턴 순환회로(TSP 알고리즘 풀이)

--- JUNGOL/Silver/1681.해밀턴 순환회로/해밀턴 순환회로.java
+++ JUNGOL/Silver/1681.해밀턴 순환회로/해밀턴 순환회로.java
@@ -1,17 +1,18 @@
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 import static java.lang.System.in;
 import static java.lang.System.out;
 
-public class Main {
+public class Main2 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(out));
     static StringTokenizer st;
+    static final int INF = 13 * 100 + 1;
     static int N;
-    static int ans = 13 * 100 + 1;
     static int[][] W;
-    static boolean[] visited;
+    static int[][] dp;
 
     public static void main(String[] args) throws IOException {
         input();
@@ -21,42 +22,55 @@ public class Main {
 
     static void input() throws IOException {
         N = Integer.parseInt(br.readLine());
-        W = new int[N+1][N+1];
-        visited = new boolean[N+1];
 
-        for(int i = 1; i <= N; i++) {
+        W = new int[N][N];
+        dp = new int[N][1 << N];
+
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 1; j <= N; j++) {
+            for (int j = 0; j < N; j++) {
                 W[i][j] = Integer.parseInt(st.nextToken());
             }
+            Arrays.fill(dp[i], -1);
         }
+
     }
 
     static void solve() {
-        dfs(1,1,0);
+        tsp(0,1);
     }
 
-    static void dfs(int cur, int depth, int cost) {
-        if (cost >= ans) return;
+    static int tsp(int prev, int bit) {
 
-        if (depth == N) {
-            if (W[cur][1] == 0) return;
-            ans = Math.min(ans, cost+W[cur][1]);
-            return;
+        // 모두 방문했을 경우
+        if (bit == (1 << N) - 1) {
+            if (W[prev][0] == 0) return INF;
+            return W[prev][0];
         }
 
-        for (int next = 2; next <= N; next++) {
-            if (visited[next]) continue;
-            if (W[cur][next] == 0) continue;
+        // 현재 이 상태로 최적의 길이 있다면
+        if (dp[prev][bit] != -1) return dp[prev][bit];
 
-            visited[next] = true;
-            dfs(next, depth + 1, cost + W[cur][next]);
-            visited[next] = false;
+        // 최대값으로 초기화
+        dp[prev][bit] = INF;
+
+        // 방문할 도시들을 탐색
+        for (int next = 0; next < N; next++) {
+
+            // 방문할 수 있는 곳인지
+            if ((bit & (1 << next)) != 0) continue;
+
+            // 이동이 불가능한 경우
+            if (W[prev][next] == 0) continue;
+
+
+            dp[prev][bit] = Math.min(dp[prev][bit], tsp(next, (bit | (1 << next))) + W[prev][next]);
         }
+
+        return dp[prev][bit];
     }
-
     static void output() throws IOException {
-        bw.write(ans + "\n");
+        bw.write(dp[0][1] + "\n");
         bw.flush();
     }
 }
