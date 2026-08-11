542ce7093ac99c465216b86cb761f655c62b8470
Huiwoong Choi <95081400+chw0912@users.noreply.github.com>
2026-08-11 16:41:31 +0900

    [level 1] Title: 대충 만든 자판, Time: 0.58 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
@@ -2,7 +2,7 @@ def solution(keymap, targets):
     result = []
     keymap = [list(keymap[i]) for i in range(len(keymap))]
     test = {}
-    # keymap 원소들을 {키 : 순서} 형태로 딕셔너리에 저장
+    
     for i in range(len(keymap)):
         for turn, key in enumerate(keymap[i]):
             if key not in test:
@@ -11,7 +11,7 @@ def solution(keymap, targets):
                 test[key] = turn + 1
                     
     
-    # target을 만들기 위한 최소 횟수 구하기
+    
     for i in range(len(targets)):
         cnt = 0
         for target in targets[i]:
